96e152090553469517767623eff7452aaee7d9ec
Nicolai Dodeac <Mykola.Dodiak@gmail.com>
2024-07-06 14:16:48 +0100

    done

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,33 @@
+// Напиши скрипт, який змінює колір фону елемента <body> через інлайн-стиль по кліку на button.change-color і задає це значення кольору текстовим вмістом для span.color.
+
+// <div class="widget">
+//   <p>Background color: <span class="color">-</span></p>
+//   <button type="button" class="change-color">Change color</button>
+// </div>
+
+// Для генерування випадкового кольору використовуй функцію getRandomHexColor().
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+const colorChangeBtnRef = document.querySelector('.change-color');
+const textChangeRef = document.querySelector('.color');
+
+colorChangeBtnRef.addEventListener('click', onTextColorInsert);
+
+function onTextColorInsert() {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  textChangeRef.textContent = newColor;
+  textChangeRef.style.backgroundColor = newColor;
+}
+
+// Зверни увагу, що функція getRandomHexColor() повертає колір у hex-форматі, в той час як колір фону на <body> буде у форматі rgb. Це нормально й не потребує якихось правок.
+
+// На що буде звертати увагу ментор при перевірці:
+
+// Фон на <body> задається тільки після кліку на button.change-color
+// При кожному кліку на елемент button.change-color фон <body> зафарбовується новим рандомним кольором
+// На <body> і span.color значення одного й того самого кольору
